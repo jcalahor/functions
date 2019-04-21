b991e3a3d22ade63dcea423d96c6b1f849ce99f2
Edgar Sánchez G <edgar.sanchez@logicstudio.net>
2019-04-21 12:05:43 -0500

    First F# version
    This version emulates in as much as possible the functionality proposed in functions.py. Main details:
    1. evalExpression(), the eval() counterpart, works with any string with a valid lambda defining *one argument*, the arg and the result could be of any type. It is strongly typed which has both pros and cons compared to Python eval().
    2. The function decorator is implemented using a simple decorator pattern (showStatus receives the function to execute as parameter).
    3. We use folds to implement the behavior *without* variables
    4. There are similar copies of the code on interactive.fsx (so you can play interactively with the code, á la Python) and Program.fs, the compilable and runnable version.
    
    Created with .NET Framework 4.7.2, but it should work with .NET 4.6.1 and above

---
+++ FunEvaluatorFS/FunEvaluatorFS/Program.fs
@@ -0,0 +1,38 @@
+﻿open FSharp.Compiler.Interactive.Shell
+open System
+open System.IO
+open System.Text
+
+let fsiSession = 
+    let inStream = new StringReader("")
+    let outStream = new StringWriter(StringBuilder())
+    let errStream = new StringWriter(StringBuilder())
+    let allArgs = [| "C:\\fsi.exe"; "--noninteractive" |]
+
+    let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
+    FsiEvaluationSession.Create(fsiConfig, allArgs, inStream, outStream, errStream) 
+
+let evalExpression<'T> text =
+  match fsiSession.EvalExpression text with
+  | Some value -> value.ReflectionValue |> unbox<'T>
+  | None       -> failwith "Couldn't evaluate the expression"
+
+let calc lambda nums =
+    let f = evalExpression<int -> int> ("fun " + lambda)
+
+    let m1 v = v + f 1
+    let m2 v = v * f 10
+    let m3 v = v + f 100
+
+    let showStatus v m =
+        printfn "Executing %s current value is %d" (m.GetType().Name) v
+        m v
+
+    let context results n = 
+        List.fold showStatus n [m1; m2; m3] :: results
+
+    nums |> List.fold context [] |> List.rev |> printfn "%A"
+
+printf "Enter lambda to use: "
+let lambdaString = Console.ReadLine()
+calc lambdaString [ 10; 11; 13 ]
